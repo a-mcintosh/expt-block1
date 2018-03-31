f7428c777c583fb79eda7ce97324bc1722fa8169
Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>
2018-03-31 15:03:33 -0500

    Ready to submit to GitHub.
    Signed-off-by: Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>

--- src/profmac_1.cpp
+++ src/profmac_1.cpp
@@ -2,20 +2,21 @@
 //  --  Goal:  Read a coinbase, manipulate it, and re-emit it.
 //  --    An experiment toward implementing a peer mining pool.
 //  --
-//  --  This code re-creates de-novo Block 1 on the main bitcoin chain,
-//  --  transforms it to the getrawtransaction hex result,
-//  --  recovers a txn from that hex, (main goal of this exercise)
-//  --  edits the recovered txn,
-//  --  and documents it all in a JSON return structure.
+//  --  This code
+//  --    re-creates de-novo Block 1 on the main bitcoin chain,
+//  --    transforms it to the getrawtransaction hex result,
+//  --    recovers a txn from that hex, (main goal of this exercise)
+//  --    edits the recovered txn,
+//  --    and documents it all in a JSON return structure.
 //  --------------------------------------------------------------
 
 //  --------------------------------------------------------------
 #include "uint256.h"
-#include "profmac_0.h"
-#include "profmac_1.h"
 #include "init.h"
 #include "bitcoinrpc.h"
 #include "build.h"
+#include "profmac_0.h"
+#include "profmac_1.h"
 
 //  --------------------------------------------------------------
 
@@ -30,10 +31,12 @@ extern void TxToJSON(const CTransaction& tx, const uint256 hashBlock, json_spiri
 //  --------------------------------------------------------------
 bool bAuthentic_block_1 = false;  //toggle some exploratory behavior
 //  --------------------------------------------------------------
-string knownPayoutAddress = "12c6DSiU4Rq3P4ZxziKxzrL5LmMBrzjrJX";
 CBigNum bnExtraNonce = 3;
 unsigned int nBits = 0x1d00ffff;
+std::vector<unsigned char> reservekey_GetReservedKey = ParseHex("0496b538e853519c726a2c91e61ec11600ae1390813a627c66fb8be7947be63c52da7589379515d4e0a604f8141781e62294721166bf621e73a82cbf2342c858ee"); 
+string knownPayoutAddress = "12c6DSiU4Rq3P4ZxziKxzrL5LmMBrzjrJX";
 //  --------------------------------------------------------------
+//  -- http://gobittest.appspot.com/Address was interesting while I was working on this.
 //  -- END USER CUSTOMIZATION STUFF
 //  --------------------------------------------------------------
 
@@ -52,18 +55,11 @@ json_spirit::Value expt_block1(const json_spirit::Array& params, bool fHelp)
             "Returns an json_spirit::Object containing mining-related information.");
 
 //  --------------------------------------------------------------
-//  --  dump a lot of parameters, mostly to learn how they are handled
-//  --  in the RPC system.
+//  --  Some worksite preparation & paperwork.
 //  --------------------------------------------------------------
     json_spirit::Object result;
     result.push_back(json_spirit::Pair("ProfMac", "expt_block1"));
     result.push_back(json_spirit::Pair(BUILD_DESC, BUILD_DATE));
-    result.push_back(json_spirit::Pair("params.size()", params.size()));
-    result.push_back(json_spirit::Pair("parameters", params));
-    json_spirit::Array arry;
-    for (uint ix=0; ix < params.size(); ix++) {
-      arry.push_back(params[ix].type());}
-    result.push_back(json_spirit::Pair("parameter_types",arry));
 //  --
     if (params.size() > 0) bAuthentic_block_1 = ("true" == params[0].get_str());
     if(bAuthentic_block_1) bnExtraNonce = 3;
@@ -73,7 +69,6 @@ json_spirit::Value expt_block1(const json_spirit::Array& params, bool fHelp)
 //  --  create coinbase transaction de-novo for historic block 1
 //  --    while doing final edits, I notice that
 //  --    CWallet::CreateTransaction exists.
-//  --    CWallet::SetReservedKey also.
 //  --------------------------------------------------------------
     static CReserveKey reservekey(pwalletMain);
 
@@ -88,7 +83,7 @@ json_spirit::Value expt_block1(const json_spirit::Array& params, bool fHelp)
 
     CScript scriptDeNovoPubKey;
     if(bAuthentic_block_1) {
-      scriptDeNovoPubKey = CScript() << ParseHex("0496b538e853519c726a2c91e61ec11600ae1390813a627c66fb8be7947be63c52da7589379515d4e0a604f8141781e62294721166bf621e73a82cbf2342c858ee") << OP_CHECKSIG;
+      scriptDeNovoPubKey = CScript() << reservekey_GetReservedKey << OP_CHECKSIG;
     } else {
       scriptDeNovoPubKey << reservekey.GetReservedKey() << OP_CHECKSIG;
       reservekey.KeepKey();
@@ -107,8 +102,10 @@ json_spirit::Value expt_block1(const json_spirit::Array& params, bool fHelp)
 //  --------------------------------------------------------------
     CDataStream ssTx(SER_NETWORK, PROTOCOL_VERSION);
     ssTx << txDeNovo;
-    result.push_back(json_spirit::Pair("ProfMac", "DeNovo-hex"));
+    result.push_back(json_spirit::Pair("ProfMac", "hex-DeNovo"));
     result.push_back(json_spirit::Pair("ssTx.size", ssTx.size()));
+    int unused = GetSizeOfCompactSize(ssTx.size());
+    result.push_back(json_spirit::Pair("bytes used to encode size", unused));
     string hexDeNovo = HexStr(ssTx.begin(), ssTx.end());
     result.push_back(json_spirit::Pair("hex-datastream", hexDeNovo));
 
@@ -118,19 +115,18 @@ json_spirit::Value expt_block1(const json_spirit::Array& params, bool fHelp)
 //  -- document into result
 //  --------------------------------------------------------------
     CTransaction txVerify;
-    ssTx >> txVerify;  //  -- test an idempotent operation.
-    txVerify.vout[0].nValue = 20 * COIN;
+    ssTx >> txVerify;  //  -- test that it's idempotent.
     result.push_back(json_spirit::Pair("ProfMac", "txVerify"));
     TxToJSON(txVerify, 0, result);
 
 //  --------------------------------------------------------------
-//  -- Serialize getrawtransaction format hex string.
-//  -- Success at this step is the core goal of this code.
+//  -- Serialize the getrawtransaction-format hex string.
+//  -- Success at this step is the core goal of this exercise.
 //  --------------------------------------------------------------
     ssTx.clear();
     ssTx << ParseHex(hexDeNovo);
 //  --
-    ssTx.ignore(1); //still buggy, fix before release
+    ssTx.ignore(GetSizeOfCompactSize(ssTx.size())); //still buggy, fix before release
     result.push_back(json_spirit::Pair("ProfMac", "trying to grok hex"));
     result.push_back(json_spirit::Pair("ssTx.size", ssTx.size()));
     hexDeNovo = HexStr(ssTx.begin(), ssTx.end());
@@ -142,34 +138,20 @@ json_spirit::Value expt_block1(const json_spirit::Array& params, bool fHelp)
     CTransaction txRecover;
     ssTx >> txRecover;  //  -- test an idempotent operation.
     txRecover.vout[0].nValue = 30 * COIN;
+    txDeNovo.vin[0].scriptSig << nBits << ++bnExtraNonce;
     
 //  --------------------------------------------------------------
-//  --  Document edits in JSON return value.
+//  --  document into result.
 //  --------------------------------------------------------------
     result.push_back(json_spirit::Pair("ProfMac", "txRecover-edited"));
     TxToJSON(txRecover, 0, result);
 
 //  --------------------------------------------------------------
-//  --  Piggybacked exploratory doodles, off-mission, don't release.
+//  --  Build newly stale block.
 //  --------------------------------------------------------------
-    ssTx.clear();
-    ssTx << ParseHex("01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0c022f030101062f503253482fffffffff070165cd1d000000001976a91459524640e26292e52f06b8d4aefb342bf3dad02a88acc3194e7e000000001976a91405bdf8f93d0ec3aea163727f90a869d201bf18fe88ac860b310e000000001976a9146a4919132c79c83c4c5cd1667709df33cb9cc0ae88ac49914915000000001976a9149cea2b01fe9557ecc471abaadbc903993ae3d67488ac0c17621c000000001976a914d50554c616c019f23c20149c4aa9f22f047e2e2888accf9c7a23000000001976a914d8ce8783e108ce285edde3805575cce75a43beed88ac9222932a000000001976a914ed7f8072c69613b15918e99413e950e6954a52e688ac00000000");
-    ssTx.ignore(0); 
-    result.push_back(json_spirit::Pair("ProfMac", "bytecoin-hexStr"));
-    result.push_back(json_spirit::Pair("ssTx.size", ssTx.size()));
-    hexDeNovo = HexStr(ssTx.begin(), ssTx.end());
-    result.push_back(json_spirit::Pair("bytecoin", hexDeNovo));
 
-    ssTx >> txRecover;  //  -- test an idempotent operation.
-    result.push_back(json_spirit::Pair("ProfMac", "bytecoin-txn"));
-    TxToJSON(txRecover, 0, result);
-    
 //  --------------------------------------------------------------
 //  --  Submit to network
-//  --------------------------------------------------------------
-
-//  --------------------------------------------------------------
-//  --  Pro-forma template to be metamorphasized.
 //  --------------------------------------------------------------
 
     return result;
